4b0c5ac3b67749d7f4f799e7c7cb7899e8ef0391
catastrophe <catherine.olivia.lee@gmail.com>
2025-05-14 22:15:45 -0700

    music suggestion placement

--- internal/ui/flutter/lib/components/music/music_section.dart
+++ internal/ui/flutter/lib/components/music/music_section.dart
@@ -577,7 +577,7 @@ class _MusicSectionState extends State<MusicSection> {
               child: !_isAuthenticated
                   ? _buildAuthPrompt()
                   : ScrollContentWrapper(
-                      headerHeight: 150, // Original value
+                      headerHeight: 145, // Original value
                       child: _buildAuthenticatedView(),
                     ),
             ),
