8a6af659c33e4bced23e876463decbfc3daf6752
catastrophe <catherine.olivia.lee@gmail.com>
2025-05-20 15:10:53 -0700

    Llama-3-7B 4_K_S best contender

--- internal/ui/flutter/lib/services/model_constants.dart
+++ internal/ui/flutter/lib/services/model_constants.dart
@@ -3,7 +3,7 @@
 /// The filename for the Mistral model
 const String kMistralModelFileName = "Mistral-7B-Instruct-v0.3-q4_1.gguf";
 
-const String kLlamaModelFileName = "Meta-Llama-3-7B-29Layers.IQ3_M.gguf";
+const String kLlamaModelFileName = "Meta-Llama-3-7B-29Layers.Q4_K_S.gguf";
 
 /// Directory name where models are stored
 const String kModelsDirectoryName = "models"; 
